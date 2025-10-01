d21fa2a426ff2e89b39583fa194a7816298b6499
gamendegamer321 <gamendegamer321@gmail.com>
2025-10-01 10:56:09 +0200

    Should be Camera and Generator

---
+++ Assets/DONT TOUCH/Scripts/BlockComponents/CameraComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSchematicComponent : SchematicBlock
+{
+    [Tooltip("The type of the camera")] public CameraType Type;
+
+    [Tooltip("The label shown to SCP-079")]
+    public string Label;
+
+    [Header("Constraints")] [Tooltip("The constraint to prevent the user from looking too far up/down")]
+    public Vector2 VerticalConstraint;
+
+    [Tooltip("The constraint to prevent the user from looking too far left/right")]
+    public Vector2 HorizontalConstraint;
+
+    [Tooltip("The constraint to prevent the user from zooming too far in/out")]
+    public Vector2 ZoomConstraint;
+
+    public override BlockType BlockType => BlockType.Camera;
+
+    public override void Compile(SchematicBlockData block)
+    {
+        block.Properties = new Dictionary<string, object>
+        {
+            { "CameraType", Type },
+            { "VerticalConstraint", VerticalConstraint },
+            { "HorizontalConstraint", HorizontalConstraint },
+            { "ZoomConstraint", ZoomConstraint },
+            { "Label", Label }
+        };
+
+        base.Compile(block);
+    }
+}
---
+++ Assets/DONT TOUCH/Scripts/BlockComponents/GeneratorComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorComponent : SchematicBlock
+{
+    [Tooltip("The permissions required to unlock the generator")]
+    public KeycardPermissions Permissions;
+
+    [Tooltip("The time it takes for the generator to be activated")]
+    public float ActivationTime;
+
+    [Tooltip("The time it takes the generator to completely decharge after its disabled")]
+    public float DeactivationTime;
+
+    [Tooltip("Whether the generator is spawned open")]
+    public bool Open;
+
+    [Tooltip("Whether the generator is unlocked when spawned")]
+    public bool Unlocked;
+
+    [Tooltip("Whether the generator is already engaged when spawned")]
+    public bool Engaged;
+
+    public override BlockType BlockType => BlockType.Generator;
+
+    public override void Compile(SchematicBlockData block)
+    {
+        block.Properties = new Dictionary<string, object>
+        {
+            { "RequiredPermissions", Permissions },
+            { "ActivationTime", ActivationTime },
+            { "DeactivationTime", DeactivationTime },
+            { "IsOpen", Open },
+            { "IsUnlocked", Unlocked },
+            { "Engaged", Engaged }
+        };
+
+        base.Compile(block);
+    }
+}
